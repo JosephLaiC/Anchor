180063d8d3254a2037bd41a8c67c1ae3e7c30fa4
Joseph Lai <35094752+JosephLaiC@users.noreply.github.com>
2024-04-28 16:41:30 +0800

    comment

--- preprocess/sequence/create_largespace_tomax_top4_rank.pl
+++ preprocess/sequence/create_largespace_tomax_top4_rank.pl
@@ -1,106 +1,84 @@
 #!/usr/bin/perl
-#
-#
-#@mat=glob "/state4/gyuanfan/TF_model/data/tf_ru_max_top4_rank/*";
-#system "mkdir /state4/gyuanfan/TF_model/data/tf_ru_max_top4_rank_largespace";
 
-$dir_input=$ARGV[0];
-$dir_output=$ARGV[1];
+# Read input and output directories from command-line arguments
+$dir_input = $ARGV[0];
+$dir_output = $ARGV[1];
 
-@mat=glob "$dir_input/*";
-system "mkdir -p $dir_output";
+# Read all files from the input directory
+@mat = glob "$dir_input/*";
 
-foreach $file (@mat){
-	open OLD, "$file" or die;
-	@all1=();
-	@all2=();
-	@all3=();
-	@all4=();
-	while ($line=<OLD>){
-		chomp $line;
-		@table=split "\t", $line;
-		push @all1, $table[0];
-		push @all2, $table[1];
-		push @all3, $table[2];
-		push @all4, $table[3];
-	}
-	close OLD;
-	@t=split '/', $file;
-	$name=pop @t;
+# Create the output directory if it doesn't exist
+system("mkdir", "-p", $dir_output);
 
-	#open NEW, ">/state4/gyuanfan/TF_model/data/tf_ru_max_top4_rank_largespace/$name" or die;
-	open NEW, ">$dir_output/$name" or die;
+foreach $file (@mat) {
+    # Open the file for reading
+    open(OLD, "$file") or die;
 
-	$i=0;
-	foreach $aaa (@all1){
-		printf NEW "%.4f", $aaa;
-		$j=1;
-		while ($j<15){
-			$max=-100;
-			if ($all1[$i-$j]>$max){
-				$max=$all1[$i-$j];
-			}
-			if ($all1[$i+$j]>$max){
-				$max=$all1[$i+$j];
-			}
-			printf NEW "\t%.4f", $max;
-			$j++;
-			$j++;
-		}
+    # Initialize arrays to store the content of the file
+    @all1 = ();
+    @all2 = ();
+    @all3 = ();
+    @all4 = ();
 
+    # Read the file line by line and populate the arrays
+    while ($line = <OLD>) {
+        chomp $line;
+        @table = split("\t", $line);
+        push @all1, $table[0];
+        push @all2, $table[1];
+        push @all3, $table[2];
+        push @all4, $table[3];
+    }
 
-		printf NEW "\t%.4f", $all2[$i];
-		$j=1;
-		while ($j<15){
-			$max=-100;
-			if ($all2[$i-$j]>$max){
-				$max=$all2[$i-$j];
-			}
-			if ($all2[$i+$j]>$max){
-				$max=$all2[$i+$j];
-			}
-			printf NEW "\t%.4f", $max;
-			$j++;
-			$j++;
-		}
+    close(OLD);
 
+    # Extract the file name
+    @t = split('/', $file);
+    $name = pop @t;
 
-		printf NEW "\t%.4f", $all3[$i];
-		$j=1;
-		while ($j<15){
-			$max=-100;
-			if ($all3[$i-$j]>$max){
-				$max=$all3[$i-$j];
-			}
-			if ($all3[$i+$j]>$max){
-				$max=$all3[$i+$j];
-			}
-			printf NEW "\t%.4f", $max;
-			$j++;
-			$j++;
-		}
+    # Open the file for writing in the output directory
+    open(NEW, ">$dir_output/$name") or die;
 
+    # Process the arrays and write the output
+    $i = 0;
+    foreach $aaa (@all1) {
+        # Calculate the maximum values for each element and its neighbors
+        printf NEW "%.4f", $aaa;
+        for ($j = 1; $j < 15; $j++) {
+            $max = -100;
+            $max = $all1[$i - $j] if ($all1[$i - $j] > $max);
+            $max = $all1[$i + $j] if ($all1[$i + $j] > $max);
+            printf NEW "\t%.4f", $max;
+        }
 
-		printf NEW "\t%.4f", $all4[$i];
-		$j=1;
-		while ($j<15){
-			$max=-100;
-			if ($all4[$i-$j]>$max){
-				$max=$all4[$i-$j];
-			}
-			if ($all4[$i+$j]>$max){
-				$max=$all4[$i+$j];
-			}
-			printf NEW "\t%.4f", $max;
-			$j++;
-			$j++;
-		}
+        # Repeat the calculation for the other three arrays
+        printf NEW "\t%.4f", $all2[$i];
+        for ($j = 1; $j < 15; $j++) {
+            $max = -100;
+            $max = $all2[$i - $j] if ($all2[$i - $j] > $max);
+            $max = $all2[$i + $j] if ($all2[$i + $j] > $max);
+            printf NEW "\t%.4f", $max;
+        }
 
-		print NEW "\n";
-		$i++;
-	}
-	close NEW;
-		
+        printf NEW "\t%.4f", $all3[$i];
+        for ($j = 1; $j < 15; $j++) {
+            $max = -100;
+            $max = $all3[$i - $j] if ($all3[$i - $j] > $max);
+            $max = $all3[$i + $j] if ($all3[$i + $j] > $max);
+            printf NEW "\t%.4f", $max;
+        }
+
+        printf NEW "\t%.4f", $all4[$i];
+        for ($j = 1; $j < 15; $j++) {
+            $max = -100;
+            $max = $all4[$i - $j] if ($all4[$i - $j] > $max);
+            $max = $all4[$i + $j] if ($all4[$i + $j] > $max);
+            printf NEW "\t%.4f", $max;
+        }
+
+        print NEW "\n";
+        $i++;
+    }
+
+    close(NEW);
 }
-
-		
