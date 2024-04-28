c10de1d5e0ee6739da271b2de04938fe0bacaace
Joseph Lai <35094752+JosephLaiC@users.noreply.github.com>
2024-04-28 16:41:22 +0800

    comment

--- train/find_best_model_I.pl
+++ train/find_best_model_I.pl
@@ -1,32 +1,42 @@
 #!/usr/bin/perl
 
-
+# Define the path to the model directories
 $path1="./model/anchor/I_all/";
 
-@model_dir=glob "${path1}*";
+# Get a list of all model directories
+@model_dir = glob "${path1}*";
 
+# Iterate through each model directory
 foreach $the_dir (@model_dir){
-    print "$the_dir\n";
-    $new_dir=$the_dir;
-    $new_dir=~s/_all//g;
-    print "$new_dir\n";
+    print "$the_dir\n"; # Print the current model directory path
+
+    # Replace '_all' in the directory name with an empty string
+    $new_dir = $the_dir;
+    $new_dir =~ s/_all//g;
+
+    # Create the new directory without '_all'
     system "mkdir -p $new_dir";
-    $perf=0;
-    $model='';
-    @all_models=glob "$the_dir/*auprc.txt";
+
+    # Initialize variables for performance tracking
+    $perf = 0;
+    $model = '';
+
+    # Find the model with the highest AUPRC value
+    @all_models = glob "$the_dir/*auprc.txt";
     foreach $the_model (@all_models){
-        open OLD, "$the_model" or die;
-        $line=<OLD>;
-        if ($line>$perf){
-            $perf=$line;
-            $model=$the_model;
+        open OLD, "$the_model" or die; # Open the current model file
+        $line = <OLD>; # Read the first line (AUPRC value)
+        if ($line > $perf){
+            $perf = $line; # Update the highest AUPRC value
+            $model = $the_model; # Store the model file with the highest AUPRC value
         }
     }
-    print "$model\n";
-    $model=~s/\.auprc\.txt//g;
-    system "cp $model ${new_dir}/the_model"
+
+    print "$model\n"; # Print the model file with the highest AUPRC value
+
+    # Replace '.auprc.txt' in the model file name with an empty string
+    $model =~ s/\.auprc\.txt//g;
+
+    # Copy the model file with the highest AUPRC value to the new directory
+    system "cp $model ${new_dir}/the_model";
 }
-
-
-
-
