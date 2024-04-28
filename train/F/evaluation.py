8bbca7bdfd0e445a9da9d90503b53524c0f951df
Joseph Lai <35094752+JosephLaiC@users.noreply.github.com>
2024-04-28 16:41:21 +0800

    comment

--- train/F/evaluation.py
+++ train/F/evaluation.py
@@ -1,19 +1,39 @@
+# Import necessary libraries and modules
 from sklearn.metrics import auc
 import numpy as np
 from sklearn.metrics import precision_recall_curve
 import sklearn.metrics as metrics
 
-y=np.loadtxt('test_gs.dat')
-pred=np.loadtxt('output.dat')
+# Load test data and predictions from .dat files
+y = np.loadtxt('test_gs.dat')
+pred = np.loadtxt('output.dat')
+
+# Calculate False Positive Rate (FPR), True Positive Rate (TPR), and thresholds
 fpr, tpr, thresholds = metrics.roc_curve(y, pred, pos_label=1)
-the_auc=metrics.auc(fpr, tpr)
-F=open('auc.txt','w')
+
+# Calculate the Area Under the Curve (AUC) for ROC curve
+the_auc = metrics.auc(fpr, tpr)
+
+# Open 'auc.txt' file in write mode
+F = open('auc.txt', 'w')
+
+# Write the calculated AUC to 'auc.txt' file
 F.write('%.4f\n' % the_auc)
+
+# Close the 'auc.txt' file
 F.close()
 
-
+# Calculate precision, recall, and thresholds
 precision, recall, thresholds = precision_recall_curve(y, pred)
-the_auprc = auc(recall, precision,reorder=False)
-F=open('auprc.txt','w')
+
+# Calculate the Area Under the Precision-Recall Curve (AUPRC)
+the_auprc = auc(recall, precision, reorder=False)
+
+# Open 'auprc.txt' file in write mode
+F = open('auprc.txt', 'w')
+
+# Write the calculated AUPRC to 'auprc.txt' file
 F.write('%.4f\n' % the_auprc)
+
+# Close the 'auprc.txt' file
 F.close()
