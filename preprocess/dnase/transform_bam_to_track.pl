8467f31674ab947ace536ac31a7e9fdbb6a29595
Joseph Lai <35094752+JosephLaiC@users.noreply.github.com>
2024-04-28 16:41:21 +0800

    comment

--- preprocess/dnase/transform_bam_to_track.pl
+++ preprocess/dnase/transform_bam_to_track.pl
@@ -1,15 +1,31 @@
+#!/usr/bin/perl
 
-$path1=$ARGV[0];
-$path2=$ARGV[1];
+use strict;
+use warnings;
 
-
-@mat=glob "$path1*bam"; # input: location of the bam file
-system "mkdir -p $path2"; # output: location for the txt file
-
-foreach $file (@mat){
-	@t=split '/', $file;
-	$name=pop @t;
-        print "$name\n";
-	system "samtools depth $file > ${path2}${name}";
+# Check if the correct number of arguments are provided
+if (@ARGV != 2) {
+    die "Usage: $0 <input_path_to_bam_files> <output_path_for_text_files>\n";
 }
 
+my $path1 = $ARGV[0];
+my $path2 = $ARGV[1];
+
+# Get a list of all BAM files in the input path
+my @mat = glob "$path1*bam";
+
+# Create the output directory if it doesn't exist
+system "mkdir -p $path2";
+
+# Iterate over each BAM file
+foreach my $file (@mat) {
+    # Extract the file name
+    my @t = split '/', $file;
+    my $name = pop @t;
+
+    # Print the file name to the console
+    print "$name\n";
+
+    # Run the samtools depth command on the file and redirect the output to a text file
+    system "samtools depth $file > ${path2}${name}";
+}
