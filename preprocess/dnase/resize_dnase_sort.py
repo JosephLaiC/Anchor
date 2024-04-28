664d580999129b6db9150a92aec7a885a09955dd
Joseph Lai <35094752+JosephLaiC@users.noreply.github.com>
2024-04-28 16:41:21 +0800

    comment

--- preprocess/dnase/resize_dnase_sort.py
+++ preprocess/dnase/resize_dnase_sort.py
@@ -3,17 +3,36 @@ import cv2
 import os
 import sys
 
-#path1='/state4/hyangl/TF_model/TF_revision/data/feature/dnase_sort/'
-#path2='/state4/hyangl/TF_model/TF_revision/data/feature/dnase_sort_resize/'
+# Define the input and output paths for the files
+# path1 is the input path where the original files are located
+# path2 is the output path where the processed files will be saved
+# The input path is obtained from the first command-line argument
+# The output path is obtained from the second command-line argument
 path1 = sys.argv[1] # input
 path2 = sys.argv[2] # output
 
+# Create the output directory if it doesn't exist
 os.system('mkdir -p ' + path2)
 
-all_files=os.listdir(path1)
+# Get a list of all the files in the input directory
+all_files = os.listdir(path1)
+
+# Loop through each file in the input directory
 for the_file in all_files:
+    # Print the name of the current file being processed
     print(the_file)
-    x=np.loadtxt(path1 + the_file)
-    y=cv2.resize(x,(1,3036304),interpolation=cv2.INTER_AREA)
-    y=np.rint(y)
+
+    # Load the current file as a numpy array
+    x = np.loadtxt(path1 + the_file)
+
+    # Resize the numpy array using OpenCV's cv2.resize function
+    # The resized array is stored in the variable y
+    # The resizing is done with the interpolation method cv2.INTER_AREA
+    y = cv2.resize(x,(1,3036304),interpolation=cv2.INTER_AREA)
+
+    # Round the values in the resized array to the nearest integer
+    y = np.rint(y)
+
+    # Save the processed array to a file in the output directory
+    # The file is saved with the same name as the original file
     np.savetxt(path2 + the_file,y,fmt='%d')
