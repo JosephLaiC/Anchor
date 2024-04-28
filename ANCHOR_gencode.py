1033c1adee2b036cfc86a984c5bd5936f5effecd
Joseph Lai <35094752+JosephLaiC@users.noreply.github.com>
2024-04-28 16:41:18 +0800

    comment

--- ANCHOR_gencode.py
+++ ANCHOR_gencode.py
@@ -7,28 +7,34 @@ import argparse
 import os
 
 def get_args():
+    """
+    Parses command line arguments using argparse.
+    """
     parser = argparse.ArgumentParser(description="ANCHOR_gencode - preprocessing the gencode data",
-        epilog='\n'.join(__doc__.strip().split('\n')[1:]).strip(),
-        formatter_class=argparse.RawTextHelpFormatter)
+                                    epilog='\n'.join(__doc__.strip().split('\n')[1:]).strip(),
+                                    formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-o', '--output', default='./data/top_20', type=str,
-        help='The output file (default: ./data/top_20)')
+                        help='The output file (default: ./data/top_20)')
     args = parser.parse_args()
     return args
 
 def main():
+    """
+    The main function that runs the script.
+    """
     args = get_args()
 
+    # Calculate the 20 closest distances to a gene for each genomic coordinate
     # step15: for each genomic coordinate, calculate the 20 closest distances to a gene; 8 hours
-    # 1.the gencode (e.g. gencode.v19.annotation.gtf) is required
-    # (downloaded from: https://www.gencodegenes.org/releases/19.html)
-    # 2.the reference file the genomic coordinates excluding blacklisted regions is needed
-    # it returns a file 'top_20' - the top 20 closes distances to a gene of each coordinate under consideration
+    # 1. The gencode file (e.g. gencode.v19.annotation.gtf) is required
+    #        (downloaded from: https://www.gencodegenes.org/releases/19.html)
+    # 2. The reference file with the genomic coordinates excluding blacklisted regions is needed
+    # it returns a file 'top_20' - the top 20 closest distances to a gene of each coordinate under consideration
     cmd = 'perl ./preprocess/gencode/create_gene_distance_top20_uniq.pl ' + \
-        args.output + ' ' + \
-        './data/ref/gencode.v19.annotation.gtf ' + \
-        './data/ref/test_regions.blacklistfiltered.bed' 
+          args.output + ' ' + \
+          './data/ref/gencode.v19.annotation.gtf ' + \
+          './data/ref/test_regions.blacklistfiltered.bed' 
     os.system(cmd)   
 
 if __name__ == '__main__':
     main()
-
