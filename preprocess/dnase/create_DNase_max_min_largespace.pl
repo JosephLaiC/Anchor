039683ddb01e90a7c513f366d94a3e663ac4b9ef
Joseph Lai <35094752+JosephLaiC@users.noreply.github.com>
2024-04-28 16:41:26 +0800

    comment

--- preprocess/dnase/create_DNase_max_min_largespace.pl
+++ preprocess/dnase/create_DNase_max_min_largespace.pl
@@ -1,72 +1,90 @@
 #!/usr/bin/perl
 
-$path1=$ARGV[0]; # input raw read
-$path2=$ARGV[1]; # output location
+# Store the input path and output location from command-line arguments
+$path1=$ARGV[0];
+$path2=$ARGV[1];
 
+# Create the output directory if it doesn't exist
 system "mkdir -p $path2";
 
+# Get all files in the input path
 @mat=glob "${path1}/*";
 
-
 foreach $file (@mat){
-        print "$file\n";
-	open OLD, "$file" or die;
-	@all1=();
-	@all2=();
-	while ($line=<OLD>){
-		chomp $line;
-		@table=split "\t", $line;
-		push @all1, $table[0];
-		push @all2, $table[1];
-	}
-	close OLD;
-	@t=split '/', $file;
-	$name=pop @t;
+    # Print the current file being processed
+    print "$file\n";
 
-	open NEW, ">${path2}/$name" or die;
-	$i=0;
-	foreach $aaa (@all1){
-		printf NEW "%.4f", $aaa;
-		$j=1;
-		while ($j<15){
-			if (defined $all1[$i-$j]){
-				printf NEW "\t%.4f", $all1[$i-$j];
-			}else{
-				printf NEW "\t0";
-			}
+    # Open the current file for reading
+    open OLD, "$file" or die;
 
-			if (defined $all1[$i+$j]){
-				printf NEW "\t%.4f", $all1[$i+$j];
-			}else{
-				printf NEW "\t0";
-			}
-			$j++;
-			$j++;
-		}
+    # Initialize arrays to store data from the file
+    @all1=();
+    @all2=();
 
-		printf NEW "\t%.4f", $all2[$i];
-		$j=1;
-		while ($j<15){
-			if (defined $all2[$i-$j]){
-				printf NEW "\t%.4f", $all2[$i-$j];
-			}else{
-				printf NEW "\t0";
-			}
+    # Read the file line by line, split each line by tab, and store the values in the arrays
+    while ($line=<OLD>){
+        chomp $line;
+        @table=split "\t", $line;
+        push @all1, $table[0];
+        push @all2, $table[1];
+    }
 
-			if (defined $all2[$i+$j]){
-				printf NEW "\t%.4f", $all2[$i+$j];
-			}else{
-				printf NEW "\t0";
-			}
-			$j++;
-			$j++;
-		}
+    # Close the file
+    close OLD;
 
-		print NEW "\n";
-		$i++;
-	}
-	close NEW;
-		
+    # Extract the file name from the full path
+    @t=split '/', $file;
+    $name=pop @t;
+
+    # Open (or create) the output file for writing
+    open NEW, ">${path2}/$name" or die;
+
+    # Process the data and write it to the output file
+    $i=0;
+    foreach $aaa (@all1){
+        # Print the current value and its surrounding values with a step of 1
+        printf NEW "%.4f", $aaa;
+        $j=1;
+        while ($j<15){
+            if (defined $all1[$i-$j]){
+                printf NEW "\t%.4f", $all1[$i-$j];
+            }else{
+                printf NEW "\t0";
+            }
+
+            if (defined $all1[$i+$j]){
+                printf NEW "\t%.4f", $all1[$i+$j];
+            }else{
+                printf NEW "\t0";
+            }
+            $j++;
+            $j++;
+        }
+
+        # Print the corresponding value from @all2 and its surrounding values with a step of 1
+        printf NEW "\t%.4f", $all2[$i];
+        $j=1;
+        while ($j<15){
+            if (defined $all2[$i-$j]){
+                printf NEW "\t%.4f", $all2[$i-$j];
+            }else{
+                printf NEW "\t0";
+            }
+
+            if (defined $all2[$i+$j]){
+                printf NEW "\t%.4f", $all2[$i+$j];
+            }else{
+                printf NEW "\t0";
+            }
+            $j++;
+            $j++;
+        }
+
+        # Move to the next line in the output file
+        print NEW "\n";
+        $i++;
+    }
+
+    # Close the output file
+    close NEW;
 }
-
-		
